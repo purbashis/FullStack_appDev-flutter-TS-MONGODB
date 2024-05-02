6b24e5b95e73df2c0760f58295ca00c822cd2eb4
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2024-05-02 20:40:40 +0530

    FullStack_appDev-flutter-TS-MONGODB
    added dependencies

---
+++ ts-backend/build/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const console_1 = require("console");
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+//Express configuration
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.set("PORT", 3000);
+app.set("BASE_URL", "http://localhost:3000");
+//Start the server 
+try {
+    const port = app.get("PORT");
+    const baseUrl = app.get("BASE_URL");
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+catch (e) {
+    console.log(console_1.error);
+}
+exports.default = server;
+//Routes
---
+++ ts-backend/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Express } from 'express';
+import http from "http";
+import cors from "cors";
+import bodyParser from 'body-parser';
+import { error } from 'console';
+
+const app: Express = express();
+
+const server = http.createServer(app);
+
+//Express configuration
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("PORT", 3000);
+app.set("BASE_URL", "http://localhost:3000");
+
+//Start the server 
+try {
+    const port: Number = app.get("PORT");
+    const baseUrl:String = app.get("BASE_URL");
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+} catch (e) {
+    console.log(error);
+    
+}
+
+
+export default server;
+
+//Routes
+
+

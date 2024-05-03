4229d216845556b3f792e768fe91fabd0cf3b70a
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2024-05-03 23:07:01 +0530

    interface and data models

---
+++ ts-backend/src/database/models/tweet.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+import { ITweetInterface } from "../interfaces/tweet.interface";
+
+const tweetSchema = new Schema<ITweetInterface>({
+   tweetId:{type: String, required: true},
+    adminId: { type: String, required: true },
+    content: { type: String, default: "" },
+
+    createdAt: { type: String, required: true },
+
+});
+
+const TweetModel = mongoose.model<ITweetInterface>("User", tweetSchema);
+export default TweetModel;
